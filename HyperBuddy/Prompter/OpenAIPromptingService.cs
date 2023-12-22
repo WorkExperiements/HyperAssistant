1208ccf210f5ae6ecfd70063d2416315a4083cf5
Christian Caicedo <ccaicedo@credera.com>
2023-12-22 15:32:47 -0600

    Initial commit.

---
+++ HyperBuddy/Prompter/OpenAIPromptingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace HyperBuddy.Prompter;
+
+public interface IPromptingService
+{
+    string GetEndpointConfig();
+}
+public class OpenAIPromptingService : IPromptingService
+{
+    private readonly string _oaiEndpoint;
+    private readonly string _oaiKey;
+    private readonly string _oaiModelName;
+    private readonly string _azureSearchEndpoint;
+    private readonly string _azureSearchKey;
+    private readonly string _azureSearchIndex;
+    public OpenAIPromptingService(IConfiguration config) {
+        _oaiEndpoint = config["AzureOAIEndpoint"] ?? "";
+        _oaiKey = config["AzureOAIKey"] ?? "";
+        _oaiModelName = config["AzureOAIModelName"] ?? "";
+        _azureSearchEndpoint = config["AzureSearchEndpoint"] ?? "";
+        _azureSearchKey = config["AzureSearchKey"] ?? "";
+        _azureSearchIndex = config["AzureSearchIndex"] ?? "";
+    }
+    public string GetEndpointConfig()
+    {
+        return _oaiEndpoint;
+    }
+}
+
---
+++ HyperBuddy/Prompter/PromptController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HyperBuddy.Prompter
+{
+    public class PromptController(IPromptingService promptsvc) : Controller
+    {
+        private readonly IPromptingService _promptingService = promptsvc;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_promptingService.GetEndpointConfig());
+        }
+    }
+}
